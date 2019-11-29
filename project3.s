36b03d9f933e2d8970d8b4b433fb6c564c1170d3
elijah-afolabi <elijah.afolabi@bison.howard.edu>
2019-11-29 00:18:21 -0500

    Realized code had vaild instead of valid

--- project3.s
+++ project3.s
@@ -57,11 +57,11 @@ check:
    beq $s0, 9, spGap
    beq $s0, 32, spGap
    ble $s0, 47, noWntLooper
-   ble $s0, 57, vaild
+ble $s0, 57, valid #vaild
    ble $s0, 64, noWntLooper
-   ble $s0, 80, vaild
+ble $s0, 80,valid #vaild
    ble $s0, 96, noWntLooper
-   ble $s0, 112, vaild
+ble $s0, 112, valid #vaild
    bge $s0, 113, noWntLooper
 
 
